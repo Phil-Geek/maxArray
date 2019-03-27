52bc688921e1431e51e29308bee614ab361e1b4e
SA-lipengfei <3042636711@qq.com>
2019-03-27 19:47:02 +0800

    功能增加，将数组扩展为首尾相接的链

--- src/com/example/junit/MaxArray.java
+++ src/com/example/junit/MaxArray.java
@@ -2,20 +2,20 @@ package com.example.junit;
 
 public class MaxArray {
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5};
-        int reqLength = 2;
+        int[] arr = {5,5,3,4,5};
+        int reqLength = 3;
         System.out.println("最大子数组和为："+maxArr(arr,reqLength));
     }
     static int maxArr(int[] arr, int reqLength){
         int arrLength = arr.length;
         int count,count2,currSum=0,nextSum=0;
 
-        for (count=0;count<arrLength-reqLength;count++){
+        for (count=0;count<arrLength;count++){
             for (count2 = 0;count2<reqLength;count2++){
                 if (count==0){
                     currSum += arr[count+count2];
                 }
-                nextSum += arr[count+count2+1];
+                nextSum += arr[(count+count2+1)%arrLength];
             }
             if (currSum <= nextSum){
                 currSum = nextSum;
